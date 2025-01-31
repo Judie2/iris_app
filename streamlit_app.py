d09c3c82471f6cdf93e8e30b50634b7d193d17c0
Judie2 <165791410+Judie2@users.noreply.github.com>
2025-01-31 12:54:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+
+#Charger le fichier iris.csv dans un DataFrame
+df = pd.read_csv('iris.csv', delimiter=";")
+
+# Afficher les premières lignes du jeu de données
+print(df.head())
+
 
